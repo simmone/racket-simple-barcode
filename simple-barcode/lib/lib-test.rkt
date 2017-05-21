0a03fd7506d1d696f052ef780eb55471c4ee63a2
simmone <chenxiao770117@gmail.com>
2017-05-21 09:30:24 +0800

    update

--- simple-barcode/lib/lib-test.rkt
+++ simple-barcode/lib/lib-test.rkt
@@ -14,7 +14,17 @@
     (check-equal? (ean13-checksum "001234567890") 5)
     (check-equal? (ean13-checksum "001234067890") 0)
    )
-    
+
+   (test-case
+    "test-char->barstring"
+    (check-equal? (char->barstring #\1 'left_odd) "0011001")
+    (check-equal? (char->barstring #\1 'left_even) "0110011")
+    (check-equal? (char->barstring #\1 'right) "1100110")
+
+    (check-equal? (char->barstring #\9 'left_odd) "0001011")
+    (check-equal? (char->barstring #\9 'left_even) "0010111")
+    (check-equal? (char->barstring #\9 'right) "1110100")
+   )
    ))
 
 (run-tests test-lib)
--- simple-barcode/lib/lib.rkt
+++ simple-barcode/lib/lib.rkt
@@ -2,6 +2,7 @@
 
 (provide (contract-out
           [ean13-checksum (-> string? exact-nonnegative-integer?)]
+          [char->barstring(-> char? symbol? string?)]
           ))
 
 (define (ean13-checksum barcode)
@@ -34,5 +35,27 @@
           0
           (- 10 (remainder step4 10))))))
 
+(define char_bar_map
+  '#hash(
+         (#\0 . ("0001101" "0100111" "1110010"))
+         (#\1 . ("0011001" "0110011" "1100110"))
+         (#\2 . ("0010011" "0011011" "1101100"))
+         (#\3 . ("0111101" "0100001" "1000010"))
+         (#\4 . ("0100011" "0011101" "1011100"))
+         (#\5 . ("0110001" "0111001" "1001110"))
+         (#\6 . ("0101111" "0000101" "1010000"))
+         (#\7 . ("0111011" "0010001" "1000100"))
+         (#\8 . ("0110111" "0001001" "1001000"))
+         (#\9 . ("0001011" "0010111" "1110100"))))
 
-          
+(define (char->barstring ch type)
+  (let ([place
+         (cond
+          [(eq? type 'left_odd)
+           0]
+          [(eq? type 'left_even)
+           1]
+          [(eq? type 'right)
+           2])])
+    (list-ref (hash-ref char_bar_map ch) place)))
+
