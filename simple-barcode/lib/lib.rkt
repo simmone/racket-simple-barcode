21cf95992d265e94f0d9b217b1d65b379813ebcc
Chen Xiao <chenxiao770117@gmail.com>
2017-06-07 16:52:03 +0800

    update

--- simple-barcode/lib/lib.rkt
+++ simple-barcode/lib/lib.rkt
@@ -6,6 +6,10 @@
           [ean13->bar_group (-> string? (listof pair?))]
           [get-dimension (-> exact-nonnegative-integer? pair?)]
           [draw-ean13 (->* (string? path-string?) (#:color_pair pair? #:brick_width exact-nonnegative-integer?) boolean?)]
+          [pic->points (-> path-string? (listof list?))]
+          [find-threshold (-> list? exact-nonnegative-integer?)]
+          [search-barcode-on-row (-> list? (or/c exact-nonnegative-integer? #f) (or/c list? #f))]
+          [search-barcode (-> (listof list?) (or/c string? #f))]
           ))
 
 (require racket/draw)
@@ -173,3 +177,144 @@
               (loop-group (cdr group_list) (+ start_x (* (string-length hold_bar) brick_width))))))
 
     (send target save-file file_name 'png)))
+
+(define (bitmap->points img)
+  (let* ([width (send img get-width)]
+         [height (send img get-height)]
+         [bits_count (* width height 4)])
+
+    (let ([bits_bytes (make-bytes bits_count)])
+      (send img get-argb-pixels 0 0 width height bits_bytes)
+      
+      (let loop ([loop_list (bytes->list bits_bytes)]
+                 [rows '()]
+                 [cols '()])
+        (if (= (length rows) height)
+            (reverse rows)
+            (if (= (length cols) width)
+                (loop loop_list (cons (reverse cols) rows) '())
+                (loop (cdr (cdr (cdr (cdr loop_list)))) 
+                      rows
+                      (cons (+ (list-ref loop_list 1) (list-ref loop_list 2) (list-ref loop_list 3)) cols))))))))
+
+(define (pic->points pic_path)
+  (bitmap->points (make-object bitmap% pic_path)))
+
+(define (find-threshold point_rows)
+  (let ([max_value 0]
+        [min_value 765])
+    (let row-loop ([loop_row_list point_rows])
+      (if (not (null? loop_row_list))
+          (begin
+            (let col-loop ([loop_col_list (car loop_row_list)])
+              (when (not (null? loop_col_list))
+                    (cond
+                     [(> (car loop_col_list) max_value)
+                      (set! max_value (car loop_col_list))]
+                     [(< (car loop_col_list) min_value)
+                      (set! min_value (car loop_col_list))]
+                     )
+                    (col-loop (cdr loop_col_list))))
+            (row-loop (cdr loop_row_list)))
+          (floor (/ (- max_value min_value) 2))))))
+
+(define (squash-points points width)
+  (let ([min_width (ceiling (* width 0.5))])
+    (let loop ([loop_points points]
+               [last_index 0]
+               [last_value -1]
+               [same_count 0]
+               [result_list '()]
+               [index_list '()])
+
+      (if (not (null? loop_points))
+          (if (= same_count width)
+              (loop (cdr loop_points) (+ width last_index) (car loop_points) 1 (cons last_value result_list) (cons last_index index_list))
+              (if (= (car loop_points) last_value)
+                  (loop (cdr loop_points) last_index last_value (add1 same_count) result_list index_list)
+                  (if (= last_value -1)
+                      (loop (cdr loop_points) last_index (car loop_points) (add1 same_count) result_list index_list)
+                      (if (>= same_count min_width)
+                          (loop (cdr loop_points) (+ last_index same_count) (car loop_points) 1 (cons last_value result_list) (cons last_index index_list))
+                          (loop (cdr loop_points) (+ last_index same_count) (car loop_points) 1 result_list index_list)))))
+          (if (and (> same_count 0) (>= same_count min_width))
+              (cons (reverse (cons last_value result_list)) (reverse (cons last_index index_list)))
+              (cons (reverse result_list) (reverse index_list)))))))
+
+(define (guess-first-dark-width points)
+  (let loop ([points_loop points]
+             [dark_length 0])
+    (if (not (null? points_loop))
+        (if (= (car points_loop) 0)
+            (if (> dark_length 0)
+                dark_length
+                (loop (cdr points_loop) dark_length))
+            (loop (cdr points_loop) (add1 dark_length)))
+        dark_length)))
+
+(define (search-barcode-on-row points_row guess_module_width)
+  (let ([max_module_width (floor (/ (length points_row) 95))])
+    (let loop ([points points_row])
+      (if (not (null? points))
+          (if (= (car points) 1)
+              (begin
+                (when (not guess_module_width)
+                      (set! guess_module_width (guess-first-dark-width points)))
+
+                (let* ([squashed_line (squash-points points_row guess_module_width)]
+                       [squashed_cols (car squashed_line)]
+                       [squashed_positions (cdr squashed_line)]
+                       [original_str 
+                        (foldr (lambda (a b) (string-append a b)) "" (map (lambda (b) (number->string b)) points_row))]
+                       [squashed_str 
+                        (foldr (lambda (a b) (string-append a b)) "" (map (lambda (b) (number->string b)) squashed_cols))])
+
+                  (if (regexp-match #px"101[0-1]{42}01010[0-1]{42}101" squashed_str)
+                      (let ([barcode_pos (car (regexp-match-positions #px"101[0-1]{42}01010[0-1]{42}101" squashed_str))])
+                        (list
+                         guess_module_width
+                         (car barcode_pos)
+                         (substring squashed_str (car barcode_pos) (cdr barcode_pos))))
+                      (if (> (length points) guess_module_width)
+                          (loop (list-tail points guess_module_width))
+                          #f))))
+              (loop (cdr points)))
+          #f))))
+
+(define (search-barcode rows)
+  (let loop ([loop_rows rows]
+             [loop_start_pos -1]
+             [loop_barcode ""]
+             [loop_count 0]
+             [loop_module_width #f])
+    (if (= loop_count 5)
+        loop_barcode
+        (if (not (null? loop_rows))
+            (let ([result (search-barcode-on-row (car loop_rows) loop_module_width)])
+              (if result
+                  (let ([module_width (list-ref result 0)]
+                        [start_pos (list-ref result 1)]
+                        [barcode (list-ref result 2)])
+
+                    (if (and
+                         (= start_pos loop_start_pos)
+                         (string=? barcode loop_barcode))
+                        (loop
+                         (cdr loop_rows)
+                         start_pos
+                         barcode
+                         (add1 loop_count)
+                         module_width)
+                        (loop
+                         (cdr loop_rows)
+                         start_pos
+                         barcode
+                         1
+                         module_width)))
+                  (loop
+                   (cdr loop_rows)
+                   -1
+                   ""
+                   0
+                   #f)))
+            #f))))
--- simple-barcode/tests/lib-test.rkt
+++ simple-barcode/tests/lib-test.rkt
@@ -6,6 +6,9 @@
 
 (require rackunit "../lib/lib.rkt")
 
+(require racket/runtime-path)
+(define-runtime-path ean13_file "../example/barcode_ean13.png")
+
 (define test-lib
   (test-suite
    "test-ean13"
@@ -63,6 +66,142 @@
       (check-equal? (cdr dimension) 90)
       )
     )
+
+   (test-case
+    "test-find-threshold"
+    
+    (let ([points_list (pic->points ean13_file)])
+      (check-equal? (length points_list) 180)
+      (check-equal? (length (car points_list)) 236)
+
+      (check-equal? (find-threshold points_list) 382)
+    ))
+
+   (test-case
+    "test-search-barcode-on-row"
+    
+    (let ([points_row_false1 '(1 0)]
+          [points_row_true1 '(1 0 1
+                              0 1 1 0 0 0 1 
+                              0 1 0 0 1 1 1 
+                              0 0 1 1 0 0 1
+                              0 1 0 0 1 1 1
+                              0 1 1 1 1 0 1
+                              0 1 1 0 0 1 1
+                              0 1 0 1 0
+                              1 0 0 0 0 1 0
+                              1 1 0 0 1 1 0
+                              1 1 0 0 1 1 0
+                              1 0 0 0 0 1 0
+                              1 1 1 0 0 1 0
+                              1 1 1 0 1 0 0 
+                              1 0 1)]
+          [points_row_true2 '(1 0 1 0 0 1 0
+                              1 0 1
+                              0 1 1 0 0 0 1 
+                              0 1 0 0 1 1 1 
+                              0 0 1 1 0 0 1
+                              0 1 0 0 1 1 1
+                              0 1 1 1 1 0 1
+                              0 1 1 0 0 1 1
+                              0 1 0 1 0
+                              1 0 0 0 0 1 0
+                              1 1 0 0 1 1 0
+                              1 1 0 0 1 1 0
+                              1 0 0 0 0 1 0
+                              1 1 1 0 0 1 0
+                              1 1 1 0 1 0 0 
+                              1 0 1
+                              0 0 1 0 1 1 1)]
+          )
+      (check-equal? (search-barcode-on-row points_row_false1 #f) #f)
+      (let ([res (search-barcode-on-row points_row_true1 #f)])
+        (check-equal? (list-ref res 0) 1)
+        (check-equal? (list-ref res 1) 0)
+        (check-equal? (list-ref res 2) "10101100010100111001100101001110111101011001101010100001011001101100110100001011100101110100101"))
+      (let ([res (search-barcode-on-row points_row_true2 #f)])
+        (check-equal? (list-ref res 0) 1)
+        (check-equal? (list-ref res 1) 7)
+        (check-equal? (list-ref res 2) "10101100010100111001100101001110111101011001101010100001011001101100110100001011100101110100101"))
+      (let ([res (search-barcode-on-row points_row_true1 1)])
+        (check-equal? (list-ref res 0) 1)
+        (check-equal? (list-ref res 1) 0)
+        (check-equal? (list-ref res 2) "10101100010100111001100101001110111101011001101010100001011001101100110100001011100101110100101"))
+      (check-equal? (search-barcode-on-row points_row_true1 3) #f)
+    ))
+
+   (test-case
+    "test-search-barcode-on-row"
+    
+    (let* (
+           [real_row '(
+                       1 0 1 0 0 1 0
+                         1 0 1
+                         0 1 1 0 0 0 1 
+                         0 1 0 0 1 1 1 
+                         0 0 1 1 0 0 1
+                         0 1 0 0 1 1 1
+                         0 1 1 1 1 0 1
+                         0 1 1 0 0 1 1
+                         0 1 0 1 0
+                         1 0 0 0 0 1 0
+                         1 1 0 0 1 1 0
+                         1 1 0 0 1 1 0
+                         1 0 0 0 0 1 0
+                         1 1 1 0 0 1 0
+                         1 1 1 0 1 0 0 
+                         1 0 1
+                         0 0 1 0 1 1 1)]
+           [real_row2 '(
+                       1 0 1 0 0 1
+                         1 0 1
+                         0 1 1 0 0 0 1 
+                         0 1 0 0 1 1 1 
+                         0 0 1 1 0 0 1
+                         0 1 0 0 1 1 1
+                         0 1 1 1 1 0 1
+                         0 1 1 0 0 1 1
+                         0 1 0 1 0
+                         1 0 0 0 0 1 0
+                         1 1 0 0 1 1 0
+                         1 1 0 0 1 1 0
+                         1 0 0 0 0 1 0
+                         1 1 1 0 0 1 0
+                         1 1 1 0 1 0 0 
+                         1 0 1
+                         0 0 1 0 1 1 1 0)]
+           [noise_row '(
+                        1 0 1 0 0 1 0
+                          1 0 0
+                          0 1 1 0 0 0 1 
+                          0 1 0 0 1 1 1 
+                          0 0 1 1 0 0 1
+                          0 1 0 0 1 1 1
+                          0 1 1 1 1 0 1
+                          0 1 1 0 0 1 1
+                          0 1 0 1 0
+                          1 0 0 0 0 1 0
+                          1 1 0 0 1 1 0
+                          1 1 0 0 1 1 0
+                          1 0 0 0 0 1 0
+                          1 1 1 0 0 1 0
+                          1 1 1 0 1 0 0 
+                          1 0 1
+                          0 0 1 0 1 1 1)]
+           [points_list_true1 (list noise_row noise_row real_row real_row real_row real_row real_row noise_row)]
+           [points_list_true2 (list real_row real_row real_row real_row real_row noise_row)]
+           [points_list_true3 (list real_row real_row real_row real_row real_row)]
+           [points_list_false1 (list noise_row noise_row real_row real_row real_row real_row noise_row)]
+           [points_list_false2 (list noise_row noise_row real_row real_row real_row real_row noise_row real_row)]
+           [points_list_false3 (list real_row real_row real_row real_row real_row2)]
+           )
+      (check-equal? (search-barcode points_list_true1) "10101100010100111001100101001110111101011001101010100001011001101100110100001011100101110100101")
+      (check-equal? (search-barcode points_list_true2) "10101100010100111001100101001110111101011001101010100001011001101100110100001011100101110100101")
+      (check-equal? (search-barcode points_list_true3) "10101100010100111001100101001110111101011001101010100001011001101100110100001011100101110100101")
+      (check-equal? (search-barcode points_list_false1) #f)
+      (check-equal? (search-barcode points_list_false2) #f)
+      (check-equal? (search-barcode points_list_false3) #f)
+    ))
 
    ))
 
