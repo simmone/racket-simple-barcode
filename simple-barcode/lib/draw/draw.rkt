2bad96e62fec35b8723042c09c6719fcca6f1537
Chen Xiao <chenxiao770117@gmail.com>
2019-06-21 11:50:21 +0800

    update

--- simple-barcode/lib/draw/draw.rkt
+++ simple-barcode/lib/draw/draw.rkt
@@ -1,17 +1,23 @@
 #lang racket
 
+(require "lib.rkt")
 (require "png.rkt")
 
 (provide (contract-out
           [drawing (-> path-string? procedure? void?)]
           [*output_type* parameter?]
+          [*file_name* parameter?]
+          [*width* parameter?]
+          [*height* parameter?]
+          [*front_color* parameter?]
+          [*back_color* parameter?]
+          [*brick_width* parameter?]
+          [draw-png (-> path-string? procedure? void?)]
           ))
-
-(define *output_type* (make-parameter 'png))
 
 (define (drawing file_name draw-func)
   (cond
-   [(eq? (*output_type*) 'png)
+   [(eq? type 'png)
     (draw-png file_name draw-func)]
    [else
     (draw-png file_name draw-func)]
--- simple-barcode/lib/draw/png.rkt
+++ simple-barcode/lib/draw/png.rkt
@@ -1,64 +1,44 @@
 #lang racket
 
+(require racket/draw)
+
+(require "lib.rkt")
+
 (provide (contract-out
-          [set-color (-> (is-a?/c bitmap-dc%) (or/c (is-a?/c color%) string?) void?)]
-          [drawing (->* (natural? natural? path-string? procedure?)
-                        (
-                         #:color_pair (cons/c string? string?)
-                         #:brick_width natural?
-                        )
-                        void?)]
+          [draw-png (-> path-string? procedure? void?)]
           [draw-bars (-> (is-a?/c bitmap-dc%) string? #:x exact-nonnegative-integer? #:y exact-nonnegative-integer? #:bar_width exact-nonnegative-integer? #:bar_height exact-nonnegative-integer? void?)]
           [save-bars (-> (is-a?/c bitmap-dc%) path-string? boolean?)]
           ))
 
-(require racket/draw)
+(define *dc* (make-parameter #f))
 
-(define (drawing width height file_name draw-func #:color_pair [color_pair '("black" . "white")] #:brick_width [brick_width 2])
-  (let* ([front_color (car color_pair)]
-         [back_color (cdr color_pair)]
-         [x (* (add1 *quiet_zone_width*) brick_width)]
-         [y (* (add1 *top_margin*) brick_width)]
-         [bar_height (* brick_width *bar_height*)]
-         [target (make-bitmap width height)]
-         [dc (new bitmap-dc% [bitmap target])])
+(define (draw-png file_name draw-func)
+  (let* ([x (* (add1 *quiet_zone_width*) (*brick_width*))]
+         [y (* (add1 *top_margin*) (*brick_width*))]
+         [bar_height (* (*brick_width*) (*bar_height*))]
+         [target (make-bitmap (*width*) (*height*))])
 
-    (when (not (string=? back_color "transparent"))
-          (set-color dc back_color)
-          (send dc draw-rectangle 0 0 width height))
+    (parameterize
+     (
+      [dc (new bitmap-dc% [bitmap target])]
+      )
+     (when (not (string=? back_color "transparent"))
+          (set-color (*dc*) (*back_color*))
+          (send (*dc*) draw-rectangle 0 0 (*width*) (*height*)))
 
-    (set-color dc front_color)
-    (send dc set-text-foreground front_color)
-    (send dc set-font (make-font #:size-in-pixels? #t #:size (* *font_size* brick_width) #:face "Monospace" #:family 'modern))
-    dc))
+    (set-color (*front_color*))
+    (send (*dc*) set-text-foreground (*front_color*))
+    (send (*dc*) set-font (make-font #:size-in-pixels? #t #:size (* (*font_size*) (*brick_width*)) #:face "Monospace" #:family 'modern))
+    
+    (draw-func)
 
+    (send (send (*dc*) get-bitmap) save-file (*file_name*) 'png))))
 
-(define (set-color dc color)
+(define (set-color color)
   (when (not (string=? color "transparent"))
-        (send dc set-pen color 1 'solid))
+        (send (*dc*) set-pen color 1 'solid))
 
-  (send dc set-brush color 'solid))
-
-(define (draw-init width height #:color_pair [color_pair '("black" . "white")] #:brick_width [brick_width 2])
-  (let* ([front_color (car color_pair)]
-         [back_color (cdr color_pair)]
-         [x (* (add1 *quiet_zone_width*) brick_width)]
-         [y (* (add1 *top_margin*) brick_width)]
-         [bar_height (* brick_width *bar_height*)]
-         [target (make-bitmap width height)]
-         [dc (new bitmap-dc% [bitmap target])])
-
-    (when (not (string=? back_color "transparent"))
-          (set-color dc back_color)
-          (send dc draw-rectangle 0 0 width height))
-
-    (set-color dc front_color)
-    (send dc set-text-foreground front_color)
-    (send dc set-font (make-font #:size-in-pixels? #t #:size (* *font_size* brick_width) #:face "Monospace" #:family 'modern))
-    dc))
-
-(define (save-bars dc file_name)
-    (send (send dc get-bitmap) save-file file_name 'png))
+  (send (*dc*) set-brush color 'solid))
 
 (define (draw-bars dc bars #:x x #:y y #:bar_width bar_width #:bar_height bar_height)
   (let loop ([loop_list (string->list bars)]
