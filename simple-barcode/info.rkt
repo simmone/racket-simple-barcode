c4ebb7f8575501ca7785cdd8d539942d452883fb
Chen Xiao <chenxiao770117@gmail.com>
2021-02-12 08:07:54 +0800

    update

--- simple-barcode/info.rkt
+++ simple-barcode/info.rkt
@@ -4,6 +4,6 @@
   '(("simple-barcode.scrbl" (multi-page) (tool 100))))
 
 (define compile-omit-paths '("tests" "standards"))
-(define test-include-paths '("tests"))
+(define test-includes-paths '("tests"))
 
 
